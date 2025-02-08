c95ac7df6c877e44ebb8d9b95668e8c7436be854
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2025-02-08 18:58:26 +0800

    update app icob

--- app/src/main/java/tes/gojek/MainActivity.kt
+++ app/src/main/java/tes/gojek/MainActivity.kt
@@ -3,6 +3,7 @@ package tes.gojek
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,6 +15,7 @@ import tes.gojek.promos.promosMain
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             val navController = rememberNavController()
 
