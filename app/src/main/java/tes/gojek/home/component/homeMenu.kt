3cf249c3a7f05ac92c0fa4f6155bb85093f5e4d5
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2025-02-13 13:58:07 +0800

    home button update

--- app/src/main/java/tes/gojek/home/component/homeMenu.kt
+++ app/src/main/java/tes/gojek/home/component/homeMenu.kt
@@ -1,7 +1,12 @@
 package tes.gojek.home.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -21,7 +26,7 @@ import tes.gojek.R
 import tes.gojek.ui.theme.placeholder
 
 @Composable
-fun menuButton(modifier: Modifier = Modifier) {
+fun MenuButton(image: Int, text: String) {
     Button(onClick = { /*TODO*/ },
         modifier = Modifier
             .wrapContentSize(),
@@ -29,19 +34,53 @@ fun menuButton(modifier: Modifier = Modifier) {
         colors = ButtonDefaults.buttonColors(
             containerColor = Color.Transparent,
             contentColor = placeholder
-        )) {
+        ),
+        contentPadding = PaddingValues(0.dp)
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(painter = painterResource(id = R.drawable.image38), contentDescription =null, modifier = Modifier
-                .size(70.dp))
-            Text(text = "ini text", fontSize = 25.sp, fontWeight = FontWeight.Light)
+            Image(painter = painterResource(id = image), contentDescription =null, modifier = Modifier
+                .size(65.dp))
+            Text(text = text, fontSize = 18.sp, fontWeight = FontWeight.Light)
         }
+    }
+}
+
+@Composable
+fun MenuGroup(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(end = 10.dp, start = 10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            MenuButton(image = R.drawable.image38, text = "GoRide")
+            MenuButton(image = R.drawable.image39, text = "GoCar")
+            MenuButton(image = R.drawable.image40, text = "GoFood")
+            MenuButton(image = R.drawable.image41, text = "GoSend")
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            MenuButton(image = R.drawable.image38, text = "GoMart")
+            MenuButton(image = R.drawable.image39, text = "GoTransit")
+            MenuButton(image = R.drawable.image40, text = "GoTagihan")
+            MenuButton(image = R.drawable.image41, text = "More")
+        }
+
     }
 }
 
 @Preview
 @Composable
 private fun preview() {
-    menuButton()
+    MenuGroup()
 }
