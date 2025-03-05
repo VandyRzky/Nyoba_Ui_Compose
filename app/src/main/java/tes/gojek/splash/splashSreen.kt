686e17bfe9c32a7864cb1b6d7a9050c10418e006
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2025-03-05 11:09:00 +0800

    update

--- app/src/main/java/tes/gojek/splash/splashSreen.kt
+++ app/src/main/java/tes/gojek/splash/splashSreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.delay
 import tes.gojek.R
 import tes.gojek.ui.theme.goGoGreen
